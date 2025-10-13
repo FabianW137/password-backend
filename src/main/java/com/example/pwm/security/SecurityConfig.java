2b389d65c3ee60d9d83acb64bf626a26ff0ba411
Fabian <fabian.wiedenmeyer@gmail.com>
2025-10-13 19:24:30 +0200

    fix jwtAuthentication

--- src/main/java/com/example/pwm/security/SecurityConfig.java
+++ src/main/java/com/example/pwm/security/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,8 +23,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-
-import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -66,8 +63,8 @@ public class SecurityConfig {
         return http.build();
 
     }
-
-    private JwtAuthenticationToken jwtToAuthToken(Jwt jwt) {
+@Bean
+    JwtAuthenticationToken jwtToAuthToken(Jwt jwt) {
         // Authorities aus Standard-Scopes
         JwtGrantedAuthoritiesConverter scopes = new JwtGrantedAuthoritiesConverter();
         scopes.setAuthorityPrefix("SCOPE_");           // z. B. SCOPE_read
